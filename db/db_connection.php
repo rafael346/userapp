894d13b7aa67c31f01e8c1115e472d0cc6cb87a4
Rafael Sousa do Nascimento <rafael.sousa@startaideia.com.br>
2024-10-23 16:25:09 -0300

    feat: create db conection

---
+++ db/db_connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'usuarios_app';
+$username = 'root'; // ou o usuário do seu banco
+$password = '123456'; // a senha do seu banco
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erro ao conectar ao banco de dados: " . $e->getMessage());
+}
+?>
